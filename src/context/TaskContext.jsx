7245e8925e5cd9d49daf42e54633571e9633479f
JuanLC28 <juan.50.cristobal@gmail.com>
2022-10-14 12:13:31 -0500

    First commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,35 @@
+import { useState, createContext } from "react";
+import { tasks as data } from "../data/tasks";
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState(data);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        createTask,
+        deleteTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
